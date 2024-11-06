e9e4cce2544d67c1d96aa16eab605067a48b9069
Jaxa111 <jkoziol379@gmail.com>
2024-11-06 09:42:49 +0100

    Update pionek.py

--- Jacek/pionek.py
+++ Jacek/pionek.py
@@ -7,14 +7,22 @@ class pawn:
     def __init__(self, hex):
         self.hex = hex
 
-    def ruch(self, destHex):
-        if self.hex.isNeighbour(destHex):
-            self.hex = destHex
+    def ruch(self, gdzieide):
+        if self.hex.isNeighbour(gdzieide):
+            self.hex = gdzieide
             
-    def draw(self, A, middle, screen, r):
+    def draw(self, A, middle, screen, p):
         position = self.hex.pawnPosition(A, middle)
-        pygame.draw.circle(screen, color, position, r)
+        pygame.draw.circle(screen, color, position, p)
 
     def randomruch(self):
-        destination = random.choice(self.hex.neighbours)
-        self.ruch(destination)
+        gdzie = random.choice(self.hex.neighbours)
+        self.ruch(gdzie)
+    
+   # def poladoruchu( self, hex):
+   #     pole1 = 
+    #    pole2 =
+    #    pole3 =
+     #   pole4 = 
+      #  pole5 = 
+       # pole6 = 
