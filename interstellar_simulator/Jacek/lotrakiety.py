fb10a7a7f7ef2814f3391060796c066818969ea9
Jaxa111 <jkoziol379@gmail.com>
2024-11-21 22:44:51 +0100

    Update lotrakiety.py

--- interstellar_simulator/Jacek/lotrakiety.py
+++ interstellar_simulator/Jacek/lotrakiety.py
@@ -1,22 +1,43 @@
 import pygame
 import math
-import random
+
+file = open('rakietadane.txt').readlines()
+file = [int(line.strip()) for line in file]
+    
+xR = file[0]
+yR = file[1]
+vR = file[2]
+mR = file[3]
+vRangle = file[4]
+xP1 = file[5]
+yP1 = file[6]
+mP1 = file[7]
+xP2 = file[8]
+yP2 = file[9]
+mP2 = file[10]
+xP3 = file[11]
+yP3 = file[12]
+mP3 = file[13]
+xP4 = file[14]
+yP4 = file[15]
+mP4 = file[16]
 
 sizeX =1000
 sizeY = 1000
  
-screen = pygame.display.set_mode([sizeX, sizeY])
 pygame.init()
+screen = pygame.display.set_mode([sizeX, sizeY])
  
-class Rocket:
+class Rakieta:
     def __init__(self,xR,yR,vR,mR,angle):
         self.xR = xR
         self.yR = yR
         self.vR = vR
         self.mR = mR
         self.angle = angle
+        
     def draw(self,screen,size):
-        pygame.draw.circle(screen,(200,200,0),(self.xR,self.yR),size)
+        pygame.draw.circle(screen,(200,70,100),(self.xR,self.yR),size)
        
 class Planet:
     def __init__(self,xP,yP,mP) :
@@ -24,8 +45,24 @@ class Planet:
         self.yP = yP
         self.mP = mP
     def draw(self,screen,size):
-        pygame.draw.circle(screen,(0,200,200),(self.xP,self.yP),size)
- 
+        pygame.draw.circle(screen,(150,0,200),(self.xP,self.yP),size)
+
+def distance (xR,yR,xP,yP):
+    return math.sqrt((xR - xP)**2 + (yR - yP)**2)
+
+def gravity (mR , mP ,xR ,yR ,xP,yP):
+    G=10
+    r=distance(xR,yR,xP,yP)
+    return G*(mR*mP)/(r**2)
+
+rakieta = Rakieta(xR, yR, vR, mR, vRangle)
+listaP = [
+    Planet(xP1, yP1, mP1),
+    Planet(xP2, yP2, mP2),
+    Planet(xP3, yP3, mP3),
+    Planet(xP4, yP4, mP4)
+]
+
 running = True
  
 while running:
@@ -33,38 +70,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    rakietka = Rocket(500,500,100,5,5)
-    
-    P1 = Planet(200,900,100)
-    P2 = Planet(100,100,150)
-    P3 = Planet(900,500,100)
-    P4 = Planet(500,250,100)
+    screen.fill((0,0,0))
 
+    for planet in listaP:
+        planet.draw(screen, size=100)
 
+    rakieta.draw(screen,10)
 
-    P1.draw(screen,50)
-    P2.draw(screen,100)
-    P3.draw(screen,50)
-    P4.draw(screen,10)
-
-    rakietka.draw(screen,10)
-
+    listaF=[]
+    for planet in  listaP:
+        F = gravity(rakieta.mR, planet.mP, rakieta.xR, rakieta.yR, planet.xP, planet.yP)
+        listaF.append(F)         
     pygame.display.flip()
-
-def distance (xR,yR,xP,yP):
-    return ((xR-xP)**2 + (yR-yP)**2)**(1/2)
-
-def gravity (mR , mP ,xR ,yR ,xP,yP):
-    G=10
-    r=distance(xR,yR,xP,yP)
-    Fg=G*(mR*mP)/(r**2)
-    return Fg
- 
-listaP=[P1,P2,P3,P4]
-listaF=[]
-
-for i in  listaP:
-    F=gravity(mR , mP ,xR ,yR ,xP,yP)
-    listaF.append(F)
+print(listaF)  
 
 pygame.quit()
+
