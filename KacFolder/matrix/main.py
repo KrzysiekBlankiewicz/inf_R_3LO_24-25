c2d9e6c678eaf8ae7c90ee41445887d23bad6dbc
Kacper <83409600+Kacper246@users.noreply.github.com>
2024-11-21 11:20:34 +0100

    skibidi

--- KacFolder/matrix/main.py
+++ KacFolder/matrix/main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 sizeX =1000
 sizeY = 1000
 
@@ -21,8 +22,18 @@ class Planet:
         self.planetx = planetx
         self.planety = planety
         self.mass = mass
-    def draw(self):
-        pass
+    def draw(self,screen,size):
+        pygame.draw.circle(screen,(200,0,0),(self.planetx,self.planety),size)
+
+def distance(x1,y1,x2,y2):
+    return math.sqrt((x1+y1)**2,(x2+y2)**2)
+
+def gravity(m1,m2,x1,y1,x2,y2):
+    g =  6.67*10**(-11)
+    r = distance(x1,y1,x2,y2)
+    fg = g*m1*m2/r**2
+    return fg
+
 
 running = True
 
@@ -31,8 +42,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill((0, 0, 0))
     rakietka = Rocket(500,100,100,5,5)
-    rakietka.draw(screen,50)
+    planetka = Planet(69,811,100)
+    planetka.draw(screen,50)
+    planetka = Planet(500,400,100)
+    planetka.draw(screen,50)
+    planetka = Planet(100,30,100)
+    planetka.draw(screen,50)
+
+    rakietka.draw(screen,10)
     pygame.display.flip()
 
 pygame.quit()
