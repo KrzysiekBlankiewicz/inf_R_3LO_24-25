ad202fac990fe80061a1521fd3050252562416c5
Jaxa111 <jkoziol379@gmail.com>
2024-12-16 09:36:05 +0100

    Update ciagfibonaczi.py

--- fibb/ciagfibonaczi.py
+++ fibb/ciagfibonaczi.py
@@ -1,7 +1,18 @@
+import time as t
+
+
 def fibo(n):
     if n==1 or n==2:
         output=1
     else:
         output = fibo(n-1) + fibo(n-2)
     return output
-print (fibo(6))
+
+
+time0 = t.time()
+
+print(fibo(45))
+      
+time1 = t.time()
+
+print(time1-time0)
