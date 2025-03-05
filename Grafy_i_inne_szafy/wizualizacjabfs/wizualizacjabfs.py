9cba9b1d565dff110bb6061951956fd24f7ac92e
Jaxa111 <jkoziol379@gmail.com>
2025-03-05 23:06:11 +0100

    Update wizualizacjabfs.py

--- Grafy_i_inne_szafy/wizualizacjabfs/wizualizacjabfs.py
+++ Grafy_i_inne_szafy/wizualizacjabfs/wizualizacjabfs.py
@@ -1,29 +1,89 @@
 import math
 import pygame
 import funkcjedogry
-import pionek
-import bfs
 
 pygame.init()
- 
+
+def vertices(position, a):
+    x = position[0]
+    y = position[1]
+    v1 = (x - a/2, y + (a * math.sqrt(3))/2 )
+    v2 = (x + a/2, y + (a * math.sqrt(3))/2 )
+    v3 = (x + a, y)
+    v4 = (x + a/2, y - (a * math.sqrt(3))/2 )
+    v5 = (x - a/2, y - (a * math.sqrt(3))/2 )
+    v6 = (x - a, y)
+    return [v1, v2, v3, v4, v5, v6]
+
+class Node:
+    def __init__(self, ID ,cords):
+        self.ID = ID
+        self.cords = cords
+    def addneighbour(self, neighbour):
+        self.neighbour = neighbour
 
 a = 30
 offset = 5
 A = a + offset / math.sqrt(3)
-screenY = 500
-screenX = 500
+screenY = 1000
+screenX = 1000
 radius = 3
 middle = (screenX/2, screenY/2)
 screen = pygame.display.set_mode([screenX, screenY])
 
+a = Node("a", (500,100))
+b = Node("b", (800,300))
+c = Node("c", (500,800))
+d = Node("d", (200,300))
+e = Node("e", (800,700))
+f = Node("f", (500,500))
+g = Node("g", (500,200))
+
+nodes=[a,b,c,d,e,f]
+
+a.addneighbour([d, f, b])
+b.addneighbour([e, a])
+c.addneighbour([e])
+d.addneighbour([a])
+e.addneighbour([b, c])
+f.addneighbour([a])
+
+def bfs(start, destination):
+    steps = [[start]]  
+    id = 0
+    visited = []  
+
+    while id < len(steps):
+        path = steps[id]  
+        node = path[-1]  
+
+        if node == destination:
+            return path  
+
+        if node not in visited:
+            visited.append(node)  
+            for neighbour in node.neighbour:
+                if neighbour not in visited:
+                    steps.append(path + [neighbour])  
+
+        id += 1
+
+    return False 
+
+steps = bfs(d, f)
+print("steps:")
+if steps == False:
+    print("Can't do it")
+else:
+    for node in steps:
+        print(node.ID)
+ 
+
 listahexy = []
 
-for q in range(-radius, radius + 1):
-        for r in range(-radius, radius + 1):
-            s = -q - r
-            if abs(q) + abs(r) + abs(s) <= radius * 2:
-                hex1 = funkcjedogry.Hex(q, -q-s, s)
-                listahexy.append(hex1)
+for node in nodes:
+        v=vertices(node.cords,a)
+        pygame.draw.polygon(screen, color, v)
 
 funkcjedogry.setNeighbourhood(listahexy)
 
@@ -37,10 +97,6 @@ while running:
 
 
     screen.fill((255, 255, 255))
-
-    for h in listahexy:
-        h.draw(a, A, middle, screen)
-    
     pygame.display.flip()
     
 pygame.quit()
