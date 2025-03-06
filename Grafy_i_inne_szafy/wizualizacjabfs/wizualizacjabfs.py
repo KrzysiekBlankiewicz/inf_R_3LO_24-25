1406396864abd058940e68958bc563e6c88ccdda
Jaxa111 <jkoziol379@gmail.com>
2025-03-06 11:20:17 +0100

    Update wizualizacjabfs.py

--- Grafy_i_inne_szafy/wizualizacjabfs/wizualizacjabfs.py
+++ Grafy_i_inne_szafy/wizualizacjabfs/wizualizacjabfs.py
@@ -1,45 +1,48 @@
 import math
 import pygame
-import funkcjedogry
+import GRAHEXY
 
 pygame.init()
 
-def vertices(position, a):
+def vertices(position, l):
     x = position[0]
     y = position[1]
-    v1 = (x - a/2, y + (a * math.sqrt(3))/2 )
-    v2 = (x + a/2, y + (a * math.sqrt(3))/2 )
-    v3 = (x + a, y)
-    v4 = (x + a/2, y - (a * math.sqrt(3))/2 )
-    v5 = (x - a/2, y - (a * math.sqrt(3))/2 )
-    v6 = (x - a, y)
+    v1 = (x - l/2, y + (l * math.sqrt(3))/2 )
+    v2 = (x + l/2, y + (l * math.sqrt(3))/2 )
+    v3 = (x + l, y)
+    v4 = (x + l/2, y - (l * math.sqrt(3))/2 )
+    v5 = (x - l/2, y - (l * math.sqrt(3))/2 )
+    v6 = (x - l, y)
     return [v1, v2, v3, v4, v5, v6]
 
 class Node:
     def __init__(self, ID ,cords):
         self.ID = ID
         self.cords = cords
-    def addneighbour(self, neighbour):
-        self.neighbour = neighbour
+        self.neighbour = []
 
-a = 30
+    def addneighbour(self, neighbour):
+        self.neighbour.extend(neighbour)
+
+color = (150, 0, 200)
+l = 30
 offset = 5
-A = a + offset / math.sqrt(3)
+A = l + offset / math.sqrt(3)
 screenY = 1000
 screenX = 1000
 radius = 3
-middle = (screenX/2, screenY/2)
+middle = (screenX / 2, screenY / 2)
 screen = pygame.display.set_mode([screenX, screenY])
 
-a = Node("a", (500,100))
-b = Node("b", (800,300))
-c = Node("c", (500,800))
-d = Node("d", (200,300))
-e = Node("e", (800,700))
-f = Node("f", (500,500))
-g = Node("g", (500,200))
+a = Node("a", (500, 100))
+b = Node("b", (800, 300))
+c = Node("c", (500, 800))
+d = Node("d", (200, 300))
+e = Node("e", (800, 700))
+f = Node("f", (500, 500))
+g = Node("g", (500, 200))
 
-nodes=[a,b,c,d,e,f]
+nodes = [a, b, c, d, e, f]
 
 a.addneighbour([d, f, b])
 b.addneighbour([e, a])
@@ -77,26 +80,20 @@ if steps == False:
 else:
     for node in steps:
         print(node.ID)
- 
 
-listahexy = []
-
-for node in nodes:
-        v=vertices(node.cords,a)
-        pygame.draw.polygon(screen, color, v)
-
-funkcjedogry.setNeighbourhood(listahexy)
-
- 
 running = True
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill((0, 255, 255))
 
 
-    screen.fill((255, 255, 255))
+    for node in nodes:
+        v = vertices(node.cords, l)
+        pygame.draw.polygon(screen, color, v)
+
+
     pygame.display.flip()
-    
+
 pygame.quit()
