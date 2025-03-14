8b8bdd9c77c0e36dca1b6acf59850b135eb05f24
JzKula <jozef.kula@gmail.com>
2025-03-14 19:54:42 +0100

    Add files via upload

--- interstellar_simulator/Jozek/Rakieta.py
+++ interstellar_simulator/Jozek/Rakieta.py
@@ -1,20 +1,22 @@
-import pygame.draw
+import pygame
+import random
+import time
 import Kolory as k
 
-g = 6.67*10**-11
+g = 6.67*10**-12 #Zwiększyłem potęgę z 11 do 12, żeby ładniej wyglądało to na symulacji
 
 class Rocket:
-    def __init__(self, mass, posX, posY):
+    def __init__(self, mass, posX, posY, vel, acc):
         self.vel = (0,0)
         self.mass = mass
         self.posX = posX
         self.posY = posY
-        self.velX = 0
-        self.velY = 0
-        self.accX =0
-        self.accY = 0
+        self.velX = vel
+        self.velY = vel
+        self.accX = acc
+        self.accY = acc
         self.color = k.white
-        self.radius = 7
+        self.radius = 5
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.posX, self.posY), self.radius)
@@ -23,13 +25,13 @@ class Rocket:
         deltaX = abs(self.posX - object.posX)
         deltaY = abs(self.posY - object.posY)
         realDistance = (deltaX**2 + deltaY**2)**0.5
-        return round(realDistance)
+        return (realDistance)
 
     def grav(self, object):
         m1 = self.mass
         m2 = object.mass
         r_2 = self.distance(object)**2
-        return round(g * m1 * m2 / r_2)
+        return (g * m1 * m2 / r_2)
 
     def draw_Fwyp(self, screen, FwypX, FwypY):
         pygame.draw.line(screen, k.green, (self.posX, self.posY), (self.posX + FwypX, self.posY + FwypY), 2)
@@ -45,3 +47,34 @@ class Rocket:
     def acceleration(self, FwypX, FwypY):
         self.accX = FwypX / self.mass
         self.accY = FwypY / self.mass
+
+    def teleport_start(self, screen):
+        pygame.draw.circle(screen, k.light_blue, (self.posX, self.posY), 20)
+        pygame.draw.circle(screen, k.white, (self.posX, self.posY), 2)
+        pygame.display.update()
+        time.sleep(0.3)
+        pygame.draw.circle(screen, k.white, (self.posX, self.posY), 7)
+        pygame.display.update()
+        time.sleep(0.3)
+        pygame.draw.circle(screen, k.white, (self.posX, self.posY), 12)
+        pygame.display.update()
+
+    def teleport_end(self, screen):
+        pygame.draw.circle(screen, k.light_blue, (self.posX, self.posY), 20)
+        pygame.draw.circle(screen, k.white, (self.posX, self.posY), 2)
+        pygame.display.update()
+        time.sleep(0.3)
+        pygame.draw.circle(screen, k.white, (self.posX, self.posY), 7)
+        pygame.display.update()
+        time.sleep(0.3)
+        pygame.draw.circle(screen, k.white, (self.posX, self.posY), 12)
+        pygame.display.update()
+        time.sleep(1)
+
+    def teleport(self):
+        self.accX = 0
+        self.accY = 0
+        self.velX = 0
+        self.velY = 0
+        self.posX = random.randrange(200,1700)
+        self.posY = random.randrange(200, 800)
--- interstellar_simulator/Jozek/Symulator.py
+++ interstellar_simulator/Jozek/Symulator.py
@@ -10,16 +10,14 @@ screenY = 1000
 pygame.init()
 screen = pygame.display.set_mode((screenX, screenY))
 
-juno = r.Rocket(3625, 950, 500)
+juno = r.Rocket(3625, 950, 500, 0, 0)
 ganimedes = p.Planet(1.4819*10**23, 444, 333, k.purple, 52*2)
 kallisto = p.Planet(1.07594*10**23, 765,765, k.blue, 24*2)
 io = p.Planet(8.93*10**22, 1420,666, k.yellow, 36*2)
 europa = p.Planet(4.80*10**22, 1234, 213, k.red, 15*2)
 
-
-
 running = True
-while running == True:
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -32,25 +30,51 @@ while running == True:
     io.draw(screen)
     europa.draw(screen)
 
-
-
-    gFx = juno.grav(ganimedes) * (ganimedes.posX - juno.posX) / 1000000000000
-    gFy = juno.grav(ganimedes) * (ganimedes.posY - juno.posY) / 1000000000000
-    kFx = juno.grav(kallisto) * (kallisto.posX - juno.posX) / 1000000000000
-    kFy = juno.grav(kallisto) * (kallisto.posY - juno.posY) / 1000000000000
-    iFx = juno.grav(io) * (io.posX - juno.posX) / 1000000000000
-    iFy = juno.grav(io) * (io.posY - juno.posY) / 1000000000000
-    eFx = juno.grav(europa) * (europa.posX - juno.posX) / 1000000000000
-    eFy = juno.grav(europa) * (europa.posY - juno.posY) / 1000000000000
-
+    gFx = juno.grav(ganimedes) * (ganimedes.posX - juno.posX) / 10**12
+    gFy = juno.grav(ganimedes) * (ganimedes.posY - juno.posY) / 10**12
+    kFx = juno.grav(kallisto) * (kallisto.posX - juno.posX) / 10**12
+    kFy = juno.grav(kallisto) * (kallisto.posY - juno.posY) / 10**12
+    iFx = juno.grav(io) * (io.posX - juno.posX) / 10**12
+    iFy = juno.grav(io) * (io.posY - juno.posY) / 10**12
+    eFx = juno.grav(europa) * (europa.posX - juno.posX )/ 10**12
+    eFy = juno.grav(europa) * (europa.posY - juno.posY) / 10**12
     FwypX = gFx + kFx + iFx + eFy
     FwypY = gFy + kFy + iFy + eFy
 
     juno.draw_Fwyp(screen, FwypX, FwypY)
-
     juno.acceleration(FwypX, FwypY)
     juno.velocity()
     juno.move()
+
+#próbowałem z listą, ale coś mi nie wyszło, więc zrobiłem ręcznie :/
+
+    #czasami jak rakieta za szybko przeleci przez planetę (w jednej klatce), to program tego nie wyłapie, ale nie dokońca wiem, jak to naprawić :(
+    if juno.posX + juno.radius/2 >= ganimedes.posX - ganimedes.radius/2 and juno.posX + juno.radius/2 <= ganimedes.posX + ganimedes.radius/2:
+        if juno.posY + juno.radius/2 >= ganimedes.posY - ganimedes.radius/2 and juno.posY +juno.radius/2 <= ganimedes.posY + ganimedes.radius/2:
+            print("UWAGA! ZDERZENIE Z GANIMEDESEM! Uruchamianie teleportacji z dala od zagrożenia...")
+            juno.teleport_start(screen)
+            juno.teleport()
+            juno.teleport_end(screen)
+    if juno.posX + juno.radius/2 >= kallisto.posX - kallisto.radius/2 and juno.posX + juno.radius/2 <= kallisto.posX + kallisto.radius/2:
+        if juno.posY + juno.radius/2 >= kallisto.posY - kallisto.radius/2 and juno.posY +juno.radius/2 <= kallisto.posY + kallisto.radius/2:
+            print("UWAGA! ZDERZENIE Z KALLISTO! Uruchamianie teleportacji z dala od zagrożenia...")
+            juno.teleport_start(screen)
+            juno.teleport()
+            juno.teleport_end(screen)
+    if juno.posX + juno.radius/2 >= io.posX - io.radius/2 and juno.posX + juno.radius/2 <= io.posX + io.radius/2:
+        if juno.posY + juno.radius/2 >= io.posY - io.radius/2 and juno.posY +juno.radius/2 <= io.posY + io.radius/2:
+            print("UWAGA! ZDERZENIE Z IO! Uruchamianie teleportacji z dala od zagrożenia...")
+            juno.teleport_start(screen)
+            juno.teleport()
+            juno.teleport_end(screen)
+    if juno.posX + juno.radius/2 >= europa.posX - europa.radius/2 and juno.posX + juno.radius/2 <= europa.posX + europa.radius/2:
+        if juno.posY + juno.radius/2 >= europa.posY - europa.radius/2 and juno.posY +juno.radius/2 <= europa.posY + europa.radius/2:
+            print("UWAGA! ZDERZENIE Z EUROPĄ! Uruchamianie teleportacji z dala od zagrożenia...")
+            juno.teleport_start(screen)
+            juno.teleport()
+            juno.teleport_end(screen)
+
+
 
 
     pygame.display.flip()
