7a63dee2c624d7ed21a306e4ee95d265119b6e9c
Kacper <83409600+Kacper246@users.noreply.github.com>
2025-03-16 22:50:02 +0100

    Update graph.py

--- Grafy_i_inne_szafy/graph/graph.py
+++ Grafy_i_inne_szafy/graph/graph.py
@@ -1,87 +1,98 @@
-import math
+import pygame
+import graph as g
 
-class Node:
-    global_id = 0
-    def __init__(self, x, y):
-        self.id = Node.global_id
-        Node.global_id += 1
-        self.x = x
-        self.y = y
-        self.nghb = []
+#modes:
+idle = 0
+node_place = 1
+vertex_start = 2
+vertex_end = 3
 
-    def add_n(node):
-        if node not in self.nghb:
-            self.nghb.append(node)
-            return True
-        return False
+def mode_change(key):
+    if key == pygame.K_n:
+        return node_place
+    if key == pygame.K_s:
+        return vertex_start
+    if key == pygame.K_e:
+        return vertex_end
+    return idle
     
-    def remove_n(node):
-        if node in self.nghb:
-            self.nghb.remove(node)
-            return True
-        return False
+def handle_click(mode, builder, position):
+    x = position[0]
+    y = position[1]
+    result = False
+
+    if mode == idle:
+        builder.tempNode = None
     
-    def distance(self, x, y):
-        dx = self.x - x
-        dy = self.y - y
-        return math.sqrt(dx**2 + dy**2)
-
-
-class GraphBuilder:
-    def __init__(self, size):
-        self.nodes = []
-        self.max_x = size
-        self.max_y = size
-        self.node_radius = 10
-        self.temp_node = None
+    elif mode == node_place:
+        builder.tempNode = None
+        result = builder.new_node(x, y)
         
-    def add_node(self, node):
-        self.nodes.append(node)
-
-    def new_node(self, x, y):
-        for n in self.nodes:
-            if n.distance(x, y) < self.node_radius:
-                return False
-        new_n = Node(x, y)
-        self.add_node(new_n)
-        return new_n.id
-
-    def new_vertex(self, n):
-        if self.tempNode != None and \
-                (self.tempNode != n) and \
-                (self.tempNode not in n.nghb) and \
-                (n.nghb not in self.tempNode.nghb):
-            self.tempNode.nghb.append(n)
-            n.nghb.append(self.tempNode)
-            tempNode = None
-            return True
-        return False
-    
-    def get_node_at_pos(self, x, y):
-        for n in self.nodes:
-            if n.distance(x, y) < self.node_radius:
-                return n
-        return None
-    
-    def to_string(self):
-        s = str(len(self.nodes)) + " " + \
-            str(self.max_x) + " " + \
-            str(self.max_y)
+    elif mode == vertex_start:
+        v = builder.get_node_at_pos(x, y)
+        if v != None:
+            builder.tempNode = v
+            result = v.id
+        else:
+            result = False
+            
+    elif mode == vertex_end:
+        v = builder.get_node_at_pos(x, y)
+        if v != None:
+            if builder.new_vertex(v):
+                result = builder.to_string()
+        else:
+            result = False
+            
+    return result
         
-        for n in self.nodes:
-            s += "\n" + str(n.id) + " " + \
-                 str(n.x) + " " + \
-                 str(n.y) + " "
-            for nn in n.nghb:
-                s += str(nn.id) + " "
-        return s
-    
-    def print(self):
-        f = open("yr.txt", 'w')
-        f.writelines(self.to_string())
-        f.close()
+size = 1000
+pygame.init()
+window = pygame.display.set_mode((size, size))
+running = True
+mode = idle
 
 
+gb = g.GraphBuilder(size)
+def get_node_by_id(node_id):
+    for node in gb.nodes:
+        if node.id == node_id:
+            #print(node)
+            return node
+f=open('yr.txt', 'r')
+content = f.read()
+f.close()
+if content:
+    for x in open('yr.txt', 'r'):
+        id = x.split()[0]
+        ngh = x.split()[3:]
+        gb.new_node(int(x.split()[1]),int(x.split()[2]))
+    for x in open('yr.txt', 'r'):
+        ngh = x.split()[3:]
+        id = x.split()[0]
+        print(ngh)
+        for y in ngh:
+            get_node_by_id(int(id)).nghb.append(get_node_by_id(int(y)))
+while running:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+            
+        if e.type == pygame.KEYDOWN:
+            mode = mode_change(e.key)
+            print("in mode:" + str(mode))
+            
+        if e.type == pygame.MOUSEBUTTONUP:
+            result = handle_click(mode, gb, e.pos)
+            if result != False:
+                gb.print()
 
+    window.fill((255, 255, 255))
+    for n in gb.nodes:
+        pygame.draw.circle(window, (0,0,0), (n.x, n.y), gb.node_radius)
+        for nn in n.nghb:
+            pygame.draw.line(window, (0,0,0), (n.x, n.y), (nn.x, nn.y))
         
-        
+    pygame.display.flip()
+
+pygame.quit()
