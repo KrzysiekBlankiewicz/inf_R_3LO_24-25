f8bbbee3887aa31d57d2b16d0a11d10ccf04b753
Kacper <83409600+Kacper246@users.noreply.github.com>
2025-04-07 08:09:48 +0200

    Update sortownia_śmieci_by-kacper.py

--- Sortowanie/sortowanie czesc1/sortownia_śmieci_by-kacper.py
+++ Sortowanie/sortowanie czesc1/sortownia_śmieci_by-kacper.py
@@ -32,21 +32,3 @@ def wolosc_wyboru(lista):
 
 print(wolosc_wyboru([1,5,2,3,8,10,7]))
 
-
-def scalatainator(prawy_twix, lewy_twix):
-    wynik = []
-    i = j = 0
-    
-    while i < len(prawy_twix) and j < len(prawy_twix):
-        if lewy_twix[i] < prawy_twix[j]:
-            wynik.append(lewy_twix[i])
-            i += 1
-        else:
-            wynik.append(prawy_twix[j])
-            j += 1
-    
-    wynik.extend(lewy_twix[i:])
-    wynik.extend(prawy_twix[j:])#cos jak append tylko, ze dodaje wiecej niz jeden znak
-    return wynik
-
-print(scalatainator(wolosc_wyboru([1,5,2,3,8,10,7]),wolosc_wyboru([2,1,3,7,334,345,245,24])))
