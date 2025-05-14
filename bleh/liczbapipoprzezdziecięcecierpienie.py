7a67fd35bbefa3ffb01a023615fcaa1485cd694c
breadboss-png <piotrek.urawski@gmail.com>
2025-05-14 23:05:20 +0200

    Update liczbapipoprzezdziecięcecierpienie.py
    wda

--- bleh/liczbapipoprzezdziecięcecierpienie.py
+++ bleh/liczbapipoprzezdziecięcecierpienie.py
@@ -5,14 +5,25 @@ xx = []
 yy = []
 
 r = 100
+w = 0
+p = 1000000
 
 def pitagey(x,y):
     wyjscie = math.sqrt(x**2+y**2)
     return wyjscie
 
-for i in range(1,10000):
-    
 
+for i in range(1,p):
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    xx.append(x)
+    yy.append(y)
 
-
-
+for x in range(0,len(xx)):
+    if abs(pitagey(xx[x],yy[y])) <= 100:
+        w += 1
+ 
+pi = (4*w)/p 
+print(pi)
+#no wy miare działa
+#https://youtu.be/cuxujgpr8Zk
